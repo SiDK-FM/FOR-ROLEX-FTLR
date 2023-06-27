69debb6cb6f4d7581acd11eeaa466ee579d9cab2
SiDK <127277028+SiDK-FM@users.noreply.github.com>
2023-06-27 16:29:02 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -39,9 +39,9 @@ async def save_group(bot, message):
             return
         buttons = [[
                     InlineKeyboardButton('🌺 ʜᴇʟᴘ ✨', url=f"https://t.me/{temp.U_NAME}?start=help"),
-                    InlineKeyboardButton('✨ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴜ', url='https://t.me/filmy_men')
+                    InlineKeyboardButton('✨ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴜ', url='https://t.me/SkymoviesHDX')
                  ],[
-                    InlineKeyboardButton("ʙᴏᴛ ᴏᴡɴᴇʀ", url="t.me/syrus_143_hpy")
+                    InlineKeyboardButton("ʙᴏᴛ ᴏᴡɴᴇʀ", url="t.me/Loki_X1")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -62,9 +62,9 @@ async def save_group(bot, message):
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
                                                                            InlineKeyboardButton('🌺 ʜᴇʟᴘ ✨', url=f"https://t.me/{temp.U_NAME}?start=help"),
-                                                                           InlineKeyboardButton('✨ ᴍʏ ᴄʜᴀɴɴᴇʟ 💕', url='https://t.me/filmy_men')
+                                                                           InlineKeyboardButton('✨ ᴍʏ ᴄʜᴀɴɴᴇʟ 💕', url='https://t.me/SkymoviesHDX')
                                                                         ],[
-                                                                           InlineKeyboardButton("ʙᴏᴛ ᴏᴡɴᴇʀ", url="t.me/syrus_143_hpy")
+                                                                           InlineKeyboardButton("ʙᴏᴛ ᴏᴡɴᴇʀ", url="t.me/Loki_X1")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
